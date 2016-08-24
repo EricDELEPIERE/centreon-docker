a9b75a7ca2c9f0953618c12ccc98090091101693
matthieu-robin <info@matthieurobin.com>
2016-08-24 17:11:19 +0200

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,6 +1,6 @@
-service httpd start
 service iptables stop
 service mysql start
 service cbd start
 service centengine start
 sed -i "s/^;date.timezone =$/date.timezone = \"Europe\/Zurich\"/" /etc/php.ini |grep "^timezone" /etc/php.ini
+service httpd restart
